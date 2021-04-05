ee5b1853f0d9f4c8139abd12b26bfbbd35a54795
marco demaio <marcopablodemaio@gmail.com>
2021-04-05 06:13:44 -0300

    inicio

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+
+function App() {
+  return (
+    <div className="grid-container">
+
+      {/* ------HEADER DE LA PAGINA------ */}
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+
+      {/* -----CUERPO D ELA PAGINA----- */}
+      <main>
+        Product List
+      </main>
+
+      {/* -----FOOTER DE LA PAGINA----- */}
+      <footer>All right is reserved.</footer>
+    </div>
+  );
+}
+
+export default App;
